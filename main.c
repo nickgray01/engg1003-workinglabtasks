f7141762dc1f2d7a74ad9adef30db05399710a58
nickgray01 <61572114+nickgray01@users.noreply.github.com>
2020-03-10 22:45:06 +1100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+int kg;
+int lb;
+printf("Enter mass in kilograms\n");
+scanf("%d", &kg);
+lb = kg*2.204;
+printf("Mass in pounds %d                                                                                        \n", lb);
+    return 0;
+
+
+}

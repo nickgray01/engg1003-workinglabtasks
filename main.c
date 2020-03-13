30b65d065b062dcf94e4c389a9381b0bb794b5d3
nickgray01 <61572114+nickgray01@users.noreply.github.com>
2020-03-13 16:21:51 +1100

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,157 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-int kg;
-int lb;
-printf("Enter mass in kilograms\n");
-scanf("%d", &kg);
-lb = kg*2.204;
-printf("Mass in pounds %d                                                                                        \n", lb);
-    return 0;
+    /*int x1=0;
+    int x2=2;
+    int d;
+    int xn;
+    int xnm1 = 2;
+    d = 2;
+    int counter = 0;
+    int s = x1 + x2;
+    printf("%d\n%d\n", x1, x2);
 
+        while (counter<100){
+
+            xn = xnm1 + d;
+
+
+            xnm1 = xn;
+            counter = counter +1;
+            s = s + xn;
+                printf("%d\n", xn);
+
+
+        }
+        //s = (0.5*xn*counter);
+printf("Sum of arithemetic series is %d", s);*/
+
+
+
+
+
+
+
+
+    /*int x1 = 2;
+    int x2 = 4;
+    int k;
+    int xn = 2;
+    k = x2/x1;
+    int count = 0;
+    int n = 0;
+    printf("%d\n", xn);
+    //printf("%d", k);
+        while (count<10){
+           // xn = x2 * k;
+            xn = xn * k;
+            count = count + 1;
+
+                printf("%d\n", xn);
+                n = n + xn;
+        }
+printf("The sum of geometric series is %d", n);*/
+
+
+
+
+
+
+
+
+
+/*int x1 = 1;
+int x2 = 2;
+int xn = 1;
+int count = 0;
+int k;
+k = x2/x1;
+    if (k>1){
+                while (count<10){
+                    count = count + 1;
+                    xn = xn*k;
+                    printf("%d\n", xn);
+
+        }
+
+
+    }
+else
+while (count<100){
+        count = count + 1;
+        xn = xn*k;
+printf("%d\n", xn);
+}
+*/
+
+
+
+
+
+/*float b1 = 1;
+float b2 = 1;
+int count = 0;
+float xnm1 = -1;
+float xn;
+float xnm2 =1;
+
+
+        while (count<10){
+                count = count + 1;
+                xn = b2*xnm2 + b1*xnm1;
+                xnm2 = xnm1;
+                xnm1 = xn;
+
+                printf("%f\n", xn);
+
+
+    }
+
+*/
+/*
+
+
+
+
+
+
+
+
+
+
+*/
+/*float x = -2;
+float y = 1;
+    while (y> -1){
+        x = -2
+            while (x>2){
+
+                if (){
+                        printf # ;
+
+                else
+                        printf(" ");
+
+            }
+                x = x + 0.05
+
+
+            }
+        printf("\n")
+    y = y - 0.05
+    }
+
+
+
+
+
+*/
+
+   return 0;
 
 }
+
+
+
